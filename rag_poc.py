04dea36dd5ad4c7e9535f746ec60853d2c26657f
Pranav <pranerm007@gmail.com>
2026-01-05 23:02:53 +0530

    Initial commit for product recommendation project

---
+++ rag_poc.py
@@ -0,0 +1,10 @@
+# 1> Choose LLM
+# 2> Load Data
+# 3> Split Data
+# 4> Create Embeddings
+# 5> Store in Vector DB
+# 6> Create Retriever
+# 7> Accept User Query
+# 8> Retrieve Relevant Data
+# 9> Inject Context into Prompt
+# 10> Generate Response
